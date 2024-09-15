180976c4e0b670ec474fe1187ed9a1be3094c623
Umeda Asahi <157201911+5TANPS@users.noreply.github.com>
2024-09-15 18:33:49 +0900

    [Add] チャット画面を作成

---
+++ lib/chat_list_page.dart
@@ -0,0 +1,53 @@
+import 'package:aisai_app/chat_page.dart';
+import 'package:flutter/material.dart';
+
+class DMListPage extends StatefulWidget {
+  @override
+  _DMListPageState createState() => _DMListPageState();
+}
+
+class _DMListPageState extends State<DMListPage> {
+  // 仮のDMデータ
+  List<Map<String, dynamic>> dmList = [
+    {'name': '田中 太郎', 'icon': 'assets/image/user1.png', 'lastMessageTime': DateTime.now().subtract(Duration(minutes: 10))},
+    {'name': '山田 花子', 'icon': 'assets/image/user2.png', 'lastMessageTime': DateTime.now().subtract(Duration(hours: 1))},
+    {'name': '佐藤 次郎', 'icon': 'assets/image/user3.png', 'lastMessageTime': DateTime.now().subtract(Duration(days: 1))},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // 直近で話したユーザーをリストの一番上に並べ替える
+    dmList.sort((a, b) => b['lastMessageTime'].compareTo(a['lastMessageTime']));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DM一覧'),
+      ),
+      body: ListView.builder(
+        itemCount: dmList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage(dmList[index]['icon']),
+            ),
+            title: Text(dmList[index]['name']),
+            subtitle: Text(
+              'Last message: ${dmList[index]['lastMessageTime']}',
+              style: TextStyle(color: Colors.grey),
+            ),
+            onTap: () {
+              // DM詳細ページへ遷移する処理を書く
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ChatPage(userName: dmList[index]['name'], userImage: '',),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
---
+++ lib/chat_page.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatefulWidget {
+  final String userName;
+  final String userImage;
+
+  ChatPage({required this.userName, required this.userImage});
+
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final TextEditingController _messageController = TextEditingController();
+  final List<Map<String, dynamic>> _messages = [];
+
+  void _sendMessage() {
+    if (_messageController.text.isNotEmpty) {
+      setState(() {
+        _messages.add({
+          'message': _messageController.text,
+          'isMe': true,
+          'userImage': widget.userImage,
+        });
+        _messageController.clear();
+        // 相手からの自動応答メッセージを追加
+        Future.delayed(Duration(milliseconds: 500), () {
+          setState(() {
+            _messages.add({
+              'message': '相手のメッセージ',
+              'isMe': false,
+              'userImage': 'assets/image/user_placeholder.png',
+            });
+          });
+        });
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage(widget.userImage),
+            ),
+            SizedBox(width: 10),
+            Text(widget.userName),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _messages.length,
+              reverse: true,
+              itemBuilder: (context, index) {
+                final message = _messages[_messages.length - 1 - index];
+                return Row(
+                  mainAxisAlignment: message['isMe']
+                      ? MainAxisAlignment.end
+                      : MainAxisAlignment.start,
+                  children: [
+                    if (!message['isMe'])
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage(message['userImage']),
+                        ),
+                      ),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 14.0),
+                      margin: EdgeInsets.symmetric(vertical: 5.0),
+                      constraints: BoxConstraints(maxWidth: 200),
+                      decoration: BoxDecoration(
+                        color: message['isMe']
+                            ? Colors.blue[200]
+                            : Colors.grey[300],
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Text(message['message']),
+                    ),
+                    if (message['isMe'])
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage(message['userImage']),
+                        ),
+                      ),
+                  ],
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _messageController,
+                    decoration: InputDecoration(
+                      hintText: 'メッセージを入力...',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: _sendMessage,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
