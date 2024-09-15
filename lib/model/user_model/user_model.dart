e37a49eafe98c2b4721f96c3db2aa8531743ceee
SeinKina <109860726+SeinKina@users.noreply.github.com>
2024-09-16 02:59:48 +0900

    [Update] UserModelの変更

--- lib/model/user_model/user_model.dart
+++ lib/model/user_model/user_model.dart
@@ -1,23 +1,41 @@
+import 'package:aisai_app/model/quest_model/quest_model.dart';
+
 class UserModel {
+  String uid;
   String name;
   int age;
   String gender;
-  String bio;
+  String? bio;
   String? profileImagePath;
   String? backgroundImagePath;
+  QuestModel? questModel;
+  String? userLocation;
+  String userOccpation;
+  bool youthGroupFlag;
+  String email;
+  String password;
+
+  
 
   final List<String> genders = ['男性', '女性', 'その他'];
 
   UserModel({
+    required this.uid,
     required this.name,
     required this.age,
     required this.gender,
     required this.bio,
-    this.profileImagePath,
-    this.backgroundImagePath, required String email, required String password,
+    required this.profileImagePath,
+    required this.backgroundImagePath,
+    required this.questModel,
+    required this.userLocation,
+    required this.userOccpation,
+    required this.youthGroupFlag,
+    required this.email,
+    required this.password,
   });
 
-  set password(String password) {}
+  set setPassword(String password) {}
 
-  set email(String email) {}
+  set setEmail(String email) {}
 }
